16f4fd2a7089283cf2495a7ed33aea8e9594aff1
aslanchek <32175896+aslanchek@users.noreply.github.com>
2023-10-09 05:04:03 +0300

    tests for double

--- algorithm/test.c
+++ algorithm/test.c
@@ -1,13 +1,30 @@
 #include "quicksort.h"
+#include <math.h>
 #include <time.h>
 
 // d1 < d2 -> < 0
 // d1 = d2 -> = 0
 // d1 > d2 -> > 0
-int compar_int(const void *dp1, const void *dp2) {
+int cmp_int(const void *dp1, const void *dp2) {
     int d1 = *(int *)dp1;
     int d2 = *(int *)dp2;
     return d1 == d2 ? 0 : d1 < d2 ? -1 : 1;
+}
+
+#define EPSILON 0.00001
+int cmp_double(const void *dp1, const void *dp2) {
+    double d1 = *(double *) dp1;
+    double d2 = *(double *) dp2;
+
+    // abs(x) < E <=> x < E and x > -E
+    // x > -E <=> -x < E
+    if ( fabs(d1 - d2) < EPSILON ) {
+        return 0;
+    } else if ( d1 - d2 < 0 ) {
+        return -1;
+    } else {
+        return 1;
+    }
 }
 
 int main() {
@@ -26,7 +43,7 @@ int main() {
     }
     fprintf(stderr, "]\n");
 
-    quicksort(ARRAY, sz, sizeof(int), compar_int);
+    quicksort(ARRAY, sz, sizeof(int), cmp_int);
 
     fprintf(stderr, "----После сортировки-------\n");
 
@@ -56,7 +73,7 @@ int main() {
     }
     fprintf(stderr, "]\n");
 
-    quicksort(ARRAY, sz, sizeof(int), compar_int);
+    quicksort(ARRAY, sz, sizeof(int), cmp_int);
 
     fprintf(stderr, "----После сортировки-------\n");
 
@@ -85,13 +102,42 @@ int main() {
     }
     fprintf(stderr, "]\n");
 
-    quicksort(ARRAY, sz, sizeof(int), compar_int);
+    quicksort(ARRAY, sz, sizeof(int), cmp_int);
 
     fprintf(stderr, "----После сортировки-------\n");
 
     fprintf(stderr, "[ ");
     for (size_t i = 0; i < sz; i++) {
         fprintf(stderr, "%d ", ARRAY[i]);
+    }
+    fprintf(stderr, "]\n");
+
+    }
+
+    printf("Нажмите Enter чтобы продолжить");
+    while( getchar() != '\n' ) {};
+
+    {
+    fprintf(stderr, "-------Тест 4--------------\n");
+    fprintf(stderr, "-------До сортировки-------\n");
+
+    double ARRAY[] = {5.0, 1.2, 1.8, 5.7, 1.2, 1.0, 5.7};
+
+    size_t sz = 7;
+
+    fprintf(stderr, "[ ");
+    for (size_t i = 0; i < sz; i++) {
+        fprintf(stderr, "%.1lf ", ARRAY[i]);
+    }
+    fprintf(stderr, "]\n");
+
+    quicksort(ARRAY, sz, sizeof(double), cmp_double);
+
+    fprintf(stderr, "----После сортировки-------\n");
+
+    fprintf(stderr, "[ ");
+    for (size_t i = 0; i < sz; i++) {
+        fprintf(stderr, "%.1lf ", ARRAY[i]);
     }
     fprintf(stderr, "]\n");
 
